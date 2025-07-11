77563070a0dde732c2f07cba38ed94ec9f4672be
Rajesh kumar <r72930521@gmail.com>
2025-07-11 20:41:03 +0530

    rg_added

---
+++ Resource_group/main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.36.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {
+    
+  }
+
+  subscription_id = "4972e01e-b3c0-45e3-becf-c21112caced1"
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "example1"
+  location = "West Europe"
+}
