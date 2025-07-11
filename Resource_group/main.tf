a33988f430053c989b2033c1c657d1198f03660a
Rajesh kumar <r72930521@gmail.com>
2025-07-11 21:23:00 +0530

    rg added by ranjeet

--- Resource_group/main.tf
+++ Resource_group/main.tf
@@ -19,3 +19,7 @@ resource "azurerm_resource_group" "example" {
   name     = "example1"
   location = "West Europe"
 }
+resource "azurerm_resource_group" "example5" {
+  name     = "example6"
+  location = "West Europe"
+}
